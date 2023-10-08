7900ab4612e00968bd57ecb5fe7829fe7913a5f0
dariwan <dariwan1607@gmail.com>
2023-10-08 20:21:10 +0700

    first commit

---
+++ app/src/main/java/com/example/myapp/AboutActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityAboutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+}
